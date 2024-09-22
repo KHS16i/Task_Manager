0e664a4ba1f6cb456f4067ea89ada8ce75fb46ef
Kevin Hernandez Sandi <khs1694@hotmail.com>
2024-09-22 15:03:41 -0600

    Removiendo dos referencias innecesarias. Se detecta al realizar pruebas de Clonado de Repositorio Remoto a Local

--- TaskManager/Views/TaskMaintenance.cs
+++ TaskManager/Views/TaskMaintenance.cs
@@ -1,6 +1,4 @@
-﻿using DevExpress.XtraGrid.Columns;
-using DevExpress.XtraRichEdit.Import.OpenXml;
-using System;
+﻿using System;
 using System.Drawing;
 using System.Linq;
 using System.Windows.Forms;
